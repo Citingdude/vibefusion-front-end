d52d8bcc90d798eef19941fcf23b71e8180aaa85
citingdude <glenn.reumers@hotmail.com>
2022-03-06 11:08:00 +0100

    Install nuxt 3

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>{{ home?.data?.hero?.title }}</div>
+    <ul>
+      <li v-for="(page, index) in pages" :key="index">
+        {{ page }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const { 
+  data: home 
+} = await useFetch(
+  'http://localhost:3333/api/v1/pages/home', 
+  { pick: 
+      [
+        'title', 
+        'slug', 
+        'data'
+      ] 
+  }
+)
+
+const {
+  data: pages
+} = await useFetch(
+  'http://localhost:3333/api/v1/pages'
+)
+</script>
