3b90836ff5a9f162ee05b0a0e251a2e20091d8ec
citingdude <glenn.reumers@hotmail.com>
2022-03-09 21:48:27 +0100

    Create admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="min-h-screen">
+    <header class="bg-slate-100 px-8 py-4 flex justify-between">
+      <div>Logo</div>
+
+      <div>Account</div>
+    </header>
+
+    <main class="flex">
+      <aside class="bg-slate-200 min-h-screen flex w-2/12">
+        <div class="flex flex-col w-full">
+          <NuxtLink to="/admin" class="px-8 py-4 bg-slate-300 hover:bg-slate-400 w-full">Dashboard</NuxtLink>
+
+          <NuxtLink to="/admin/pages" class="px-8 py-4 bg-slate-300 hover:bg-slate-400">Pages</NuxtLink>
+        </div>
+      </aside>
+
+      <div class="flex w-10/12">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
