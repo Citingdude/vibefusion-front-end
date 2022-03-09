3fdcd4907120485229dbd7e216f7badf7f5ffd01
citingdude <glenn.reumers@hotmail.com>
2022-03-09 20:18:43 +0100

    Create admin index

---
+++ pages/admin/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Admin dashboard</h1>
+  </div>
+</template>
+
+<script setup>
+const { 
+  data: home 
+} = await useFetch(
+  'http://localhost:3333/api/v1/pages/home', 
+  { pick: 
+      [
+        'title', 
+        'slug', 
+        'data'
+      ] 
+  }
+)
+</script>
