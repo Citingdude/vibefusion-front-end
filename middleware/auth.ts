70ffbc7601d172de84d7233a9a14d13e23589c62
Glenn <glenn.reumers@hotmail.com>
2022-03-15 15:14:34 +0100

    Add new auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -3,9 +3,9 @@ import { store } from '@/store/store.js'
 export default defineNuxtRouteMiddleware((to, from) => {
 
   const user = useUser()
-  const auth = store.user
+  const auth = store.user.token
 
-  if (auth === false) {
+  if (! auth) {
     console.log(user.value)
 
     return navigateTo('/login')
