0b51afa9cbeb38505601b2236cfaff7470a58bde
Glenn <glenn.reumers@hotmail.com>
2022-03-15 15:14:49 +0100

    Add token and tokentype to user in store

--- store/store.js
+++ store/store.js
@@ -1,11 +1,30 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-  user: false,
-  setTrue() {
-    this.user = true
+  user: {
+    status: false,
+    token: "",
+    tokenType: "",
   },
+
+  setTrue() {
+    this.user.status = true
+  },
+
   setFalse() {
-    this.user = false
+    this.user.status = false
+  },
+
+  setToken(token) {
+    this.user.token = token
+  },
+
+  setTokenType(type) {
+    this.user.tokenType = type
+  },
+
+  logout() {
+    this.user.token = ""
+    this.user.tokenType = ""
   }
 })
