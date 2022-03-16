25bd1a5f61e7c9f80e8f32ce712b28c3c803636f
citingdude <glenn.reumers@hotmail.com>
2022-03-16 21:01:46 +0100

    Add header

---
+++ components/AppHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <header class="shadow-xl">
+    <div
+      class="container mx-auto relative grid grid-cols-12 lg:gap-12 section-padding-x py-4 lg:py-6 items-center"
+    >
+      <div class="col-span-8 lg:col-span-3 max-w-xs">
+        <NuxtLink to="/">
+          <AppLogo />
+        </NuxtLink>
+      </div>
+
+      <AppNavigation />
+
+      <div class="hidden lg:flex lg:col-span-3 justify-end">
+        <NuxtLink
+          to="/contact"
+          class="bg-light-alt px-4 py-2 rounded-tl-lg rounded-br-lg font-display font-medium text-xl text-accent transition border-2 border-light-main hover:border-accent"
+        >Contact</NuxtLink>
+      </div>
+
+      <!-- Hamburger -->
+      <div @click="toggleMobileMenu()" class="flex col-span-4 lg:hidden justify-end items-center">
+        <AppHamburger />
+      </div>
+
+      <MobileNavigation :mobileMenu="mobileMenu" />
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import AppLogo from './branding/AppLogo.vue';
+
+let mobileMenu = ref(false)
+
+function toggleMobileMenu() {
+  mobileMenu.value = !mobileMenu.value
+  // if (mobileMenu) {
+  //   setTimeout(() => setCloseEvent(), 100);
+  // }
+}
+
+// function closeMobileMenu() {
+//   mobileMenu.value = false
+//   window.removeEventListener('click', closeMobileMenu)
+// }
+
+// function setCloseEvent() {
+//   window.addEventListener('click', closeMobileMenu)
+// }
+
+</script>
---
+++ components/AppNavigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav
+    class="hidden lg:flex justify-center items-center space-x-10 col-span-6 font-display font-medium text-xl"
+  >
+    <NuxtLink to="/">Home</NuxtLink>
+
+    <div @click="toggleDropdown()" class="relative cursor-pointer">
+      <p>Diensten</p>
+      <div
+        v-if="dienstenDropdown"
+        class="absolute flex flex-col top-full p-6 bg-light-alt rounded-2xl space-y-3"
+      >
+        <NuxtLink to="/webdesign">Webdesign</NuxtLink>
+        <NuxtLink to="/webshop">Webshop</NuxtLink>
+        <NuxtLink to="/branding">Branding</NuxtLink>
+      </div>
+    </div>
+
+    <NuxtLink to="/cases">Cases</NuxtLink>
+
+    <NuxtLink to="/blog">Blog</NuxtLink>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let dienstenDropdown = ref(false)
+
+function toggleDropdown() {
+  dienstenDropdown.value = !dienstenDropdown.value
+  // if (dienstenDropdown) {
+  //   setTimeout(() => setCloseEvent(), 500);
+  // }
+}
+
+// function closeDropdown() {
+//   dienstenDropdown.value = false
+//   window.removeEventListener('click', closeDropdown)
+// }
+
+// function setCloseEvent() {
+//   window.addEventListener('click', closeDropdown)
+// }
+</script>
