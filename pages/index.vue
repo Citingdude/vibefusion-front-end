fc17f055f0f8e5b35316f503fe42f13659efd71b
citingdude <glenn.reumers@hotmail.com>
2022-03-16 20:42:39 +0100

    Add hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,11 @@
 <template>
   <div>
-    <div>{{ home?.data?.hero?.title }}</div>
-    <ul>
-      <li v-for="(page, index) in pages" :key="index">
-        {{ page }}
-      </li>
-    </ul>
+    <HomeHero />
+    <!-- <HomeDiensten />
+    <HomeCases />
+    <CallToAction /> -->
   </div>
 </template>
 
 <script setup>
-const { 
-  data: home 
-} = await useFetch(
-  'http://localhost:3333/api/v1/pages/home', 
-  { pick: 
-      [
-        'title', 
-        'slug', 
-        'data'
-      ] 
-  }
-)
-
-const {
-  data: pages
-} = await useFetch(
-  'http://localhost:3333/api/v1/pages'
-)
 </script>
