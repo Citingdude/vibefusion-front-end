fbe287eb148a15844c2668c41d929443f0c84808
citingdude <glenn.reumers@hotmail.com>
2022-03-16 20:42:29 +0100

    Add button components

---
+++ components/Buttons/ButtonsButtonLargeAlt.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLink
+    :to="props.link"
+    class="btn-transparent border-2 font-bold font-display text-xl max-w-max py-4 px-8 rounded-tl-xl rounded-br-xl btn-hover"
+  >{{ props.text }}</NuxtLink>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: 'Button'
+  },
+  link: {
+    type: Object,
+    default: '/'
+  }
+})
+</script>
