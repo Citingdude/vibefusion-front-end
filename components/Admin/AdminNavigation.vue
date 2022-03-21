fab22d8ed038335b7cb43a670741df45f97f3e4e
citingdude <glenn.reumers@hotmail.com>
2022-03-21 21:03:23 +0100

    Add box-shadow

--- components/Admin/AdminNavigation.vue
+++ components/Admin/AdminNavigation.vue
@@ -1,6 +1,6 @@
 <template>
-  <aside class="bg-slate-200 min-h-screen flex w-2/12">
-    <div class="flex flex-col w-full">
+  <aside class="bg-slate-200 min-h-screen flex w-2/12 shadow-lg">
+    <div class="flex flex-col w-full p-8 space-y-2">
       <AdminNavigationLink to="/admin">
         Dashboard
       </AdminNavigationLink>
