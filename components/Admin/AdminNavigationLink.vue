7bc0e0d95845e4da30cb411a7110a40820fea838
citingdude <glenn.reumers@hotmail.com>
2022-03-21 20:50:51 +0100

    Create AdminNavigationLink component

---
+++ components/Admin/AdminNavigationLink.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLink :to="props.to" class="px-8 py-4 bg-slate-300 hover:bg-slate-400 w-full">
+    <slot />
+  </NuxtLink>
+</template>
+
+<script setup>
+const props = defineProps({
+  to: {
+    type: String,
+    default: '/'
+  }
+})
+</script>
