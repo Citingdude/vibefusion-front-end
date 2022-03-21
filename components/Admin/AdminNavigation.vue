36507f0374c2eef3d148cb687023f698c973798a
citingdude <glenn.reumers@hotmail.com>
2022-03-21 20:50:38 +0100

    Create AdminNavigation component

---
+++ components/Admin/AdminNavigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <aside class="bg-slate-200 min-h-screen flex w-2/12">
+    <div class="flex flex-col w-full">
+      <AdminNavigationLink to="/admin">
+        Dashboard
+      </AdminNavigationLink>
+
+      <AdminNavigationLink to="/admin/pages">
+        Pages
+      </AdminNavigationLink>
+    </div>
+  </aside>
+</template>
+
+<script setup>
+
+</script>
