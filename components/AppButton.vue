9fb2422f245d3187232a440092ed11f08cd05b54
citingdude <glenn.reumers@hotmail.com>
2022-03-21 20:44:27 +0100

    Create AppButton component

---
+++ components/AppButton.vue
@@ -0,0 +1,69 @@
+<template>
+  <NuxtLink
+    v-if="!button"
+    :to="props.to"
+    class="btn-hover max-w-max rounded-tl-xl rounded-br-xl"
+    :class="getSize, getColor"
+  >
+    <slot />
+  </NuxtLink>
+
+  <button
+    @click="$emit('click')"
+    v-if="button"
+    class="btn-hover max-w-max rounded-tl-xl rounded-br-xl"
+    :class="getSize, getColor"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  button: {
+    type: Boolean,
+    default: false
+  },
+
+  to: {
+    type: String,
+    default: '/'
+  },
+
+  color: {
+    type: String,
+    default: 'accent'
+  },
+
+  size: {
+    type: String,
+    default: 'medium'
+  }
+})
+
+const emit = defineEmits(['click'])
+
+const getColor = computed(() => {
+  switch (props.color) {
+    case 'accent':
+      return 'btn-accent'
+    case 'alt':
+      return 'btn-alt'
+  }
+})
+
+const getSize = computed(() => {
+  switch (props.size) {
+    case 'small':
+      return 'btn-sm'
+    case 'medium':
+      return 'btn-md'
+    case 'large':
+      return 'btn-lg'
+  }
+})
+
+
+</script>
