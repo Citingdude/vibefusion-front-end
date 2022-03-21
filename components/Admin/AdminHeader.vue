a7ae608bce3e752ff63cf1a9bb3f29a637236825
citingdude <glenn.reumers@hotmail.com>
2022-03-21 19:57:14 +0100

    Extract admin header into component

---
+++ components/Admin/AdminHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header class="bg-slate-100 px-8 py-4 flex justify-between">
+      <AppLogo class="w-40" />
+
+      <div class="space-x-8 flex">
+        <NuxtLink to="/login">Login</NuxtLink>
+        <button @click="logout()">Logout</button>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script setup>
+
+</script>
--- layouts/admin.vue
+++ layouts/admin.vue
@@ -15,14 +15,7 @@ async function setValidToken() {
 
 <template>
   <div class="min-h-screen">
-    <header class="bg-slate-100 px-8 py-4 flex justify-between">
-      <AppLogo class="w-40" />
-
-      <div class="space-x-8 flex">
-        <NuxtLink to="/login">Login</NuxtLink>
-        <button @click="logout()">Logout</button>
-      </div>
-    </header>
+    <AdminHeader />
 
     <main class="flex">
       <aside class="bg-slate-200 min-h-screen flex w-2/12">
