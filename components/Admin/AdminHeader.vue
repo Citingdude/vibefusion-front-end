4eeb25a3df2f9da8424762a824713145f84a9378
citingdude <glenn.reumers@hotmail.com>
2022-03-21 20:44:54 +0100

    Replace buttons with new AppButton component

--- components/Admin/AdminHeader.vue
+++ components/Admin/AdminHeader.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
-    <header class="bg-slate-100 px-8 py-4 flex justify-between">
-      <AppLogo class="w-40" />
+  <header class="justify-between bg-slate-100 px-8 py-4 flex items-center">
+    <div class="w-1/2">
+      <NuxtLink to="/admin">
+        <BrandingAppLogo class="w-60" />
+      </NuxtLink>
+    </div>
 
-      <div class="space-x-8 flex">
-        <NuxtLink to="/login">Login</NuxtLink>
-        <button @click="logout()">Logout</button>
-      </div>
-    </header>
-  </div>
+    <div class="space-x-8 flex w-1/2 justify-end items-center">
+      <AppButton to="/login" size="small">Login</AppButton>
+      <AppButton @click="logout()" :button="true" to="/login" size="small" color="alt">Logout</AppButton>
+    </div>
+  </header>
 </template>
 
 <script setup>
+import { store } from "@/store/store";
 
+async function setValidToken() {
+  store.user.validToken = false;
+}
+
+async function logout() {
+  await setValidToken().then(() => {
+    return navigateTo("/login");
+  });
+}
 </script>
