00b02892ca11329af2d3d4107a06165bab332400
Glenn <glenn.reumers@hotmail.com>
2022-03-23 14:33:06 +0100

    Edit logout funciton

--- components/Admin/AdminHeader.vue
+++ components/Admin/AdminHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  <header class="justify-between bg-slate-100 px-8 py-4 flex items-center shadow-md">
+  <header
+    class="justify-between bg-slate-100 px-8 py-4 flex items-center shadow-md"
+  >
     <div class="w-1/2">
       <NuxtLink to="/admin">
         <BrandingAppLogo class="w-60" />
@@ -8,7 +10,14 @@
 
     <div class="space-x-4 flex w-1/2 justify-end items-center">
       <AppButton to="/login" size="small">Login</AppButton>
-      <AppButton @click="logout()" :button="true" to="/login" size="small" color="alt">Logout</AppButton>
+      <AppButton
+        @click="logout()"
+        :button="true"
+        to="/login"
+        size="small"
+        color="alt"
+        >Logout</AppButton
+      >
     </div>
   </header>
 </template>
@@ -16,8 +25,10 @@
 <script setup>
 import { store } from "@/store/store";
 
+const cookieToken = useCookie("token");
+
 async function setValidToken() {
-  store.user.validToken = false;
+  cookieToken.value = "false";
 }
 
 async function logout() {
