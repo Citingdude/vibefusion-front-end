62592c5e88c47e4bf01cca617a40d660943bffe3
citingdude <glenn.reumers@hotmail.com>
2022-04-01 21:44:53 +0200

    Add api data to hero title

--- components/Home/HomeHero.vue
+++ components/Home/HomeHero.vue
@@ -6,17 +6,19 @@
       <div class="grid grid-cols-2 lg:grid-cols-12 gap-12">
         <div class="col-span-2 lg:col-span-6 max-w-full items-center anim-fade-in-left">
           <h1
-            class="text-5xl md:text-6xl lg:text-7xl text-center md:text-left font-display font-bold text-dark-400 mb-8 md:mb-12 lg:mb-24 leading-tight max-w-s"
-          >
-            {{ home[0].data.hero.title }}
-          </h1>
+            v-html="home[0].data.hero.title"
+            class="hero__title text-7xl font-display mb-24"
+          ></h1>
 
           <div class="flex flex-col md:flex-row items-center md:items-start gap-8">
             <ButtonsButtonLargeMain
               :text="home[0].data.hero.button"
               :link="{ path: '/', hash: '#onze-diensten' }"
             />
-            <ButtonsButtonLargeAlt :text="home[0].data.hero.button_alt" :link="{ path: '/', hash: '#cases' }" />
+            <ButtonsButtonLargeAlt
+              :text="home[0].data.hero.button_alt"
+              :link="{ path: '/', hash: '#cases' }"
+            />
           </div>
         </div>
 
@@ -32,3 +34,9 @@
 const { data: home } = await useAsyncData('home', () => $fetch('http://localhost:3333/api/v1/pages'))
 
 </script>
+
+<style>
+.hero__title > p {
+  font-weight: normal;
+}
+</style>
