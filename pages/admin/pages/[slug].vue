cf73da7f8ac2fb8f14af5f19a73f2e2ac8c28715
citingdude <glenn.reumers@hotmail.com>
2022-04-04 19:59:39 +0200

    Upload file WIP

--- pages/admin/pages/[slug].vue
+++ pages/admin/pages/[slug].vue
@@ -12,7 +12,7 @@
               <label class="text-lg font-medium" for>Slug</label>
               <input
                 class="border border-blue-50 rounded px-3 py-2"
-                v-model="formData.slug"
+                v-model="formDataTest.slug"
                 type="text"
               />
             </div>
@@ -21,8 +21,20 @@
               <label class="text-lg font-medium" for>Title</label>
               <input
                 class="border border-blue-50 rounded px-3 py-2"
-                v-model="formData.title"
+                v-model="formDataTest.title"
                 type="text"
+                :placeholder="page.title"
+              />
+            </div>
+
+            <div class="flex flex-col shadow-sm space-y-2">
+              <label class="text-lg font-medium" for>Image</label>
+              <input
+                class="border border-blue-50 rounded px-3 py-2"
+                type="file"
+                id="file"
+                ref="file"
+                v-on:change="handleFileUpload()"
                 :placeholder="page.title"
               />
             </div>
@@ -30,16 +42,16 @@
             <h2 class="text-2xl font-semibold mb-4">Hero</h2>
 
             <div class="flex flex-col shadow-sm space-y-2">
-              <TipTap v-model="formData.data.hero.title" />
+              <TipTap v-model="formDataTest.data.hero.title" />
             </div>
 
             <div class="flex flex-col shadow-sm space-y-2">
               <label class="text-lg font-medium" for>Hero button</label>
               <input
                 class="border border-blue-50 rounded px-3 py-2"
-                v-model="formData.data.hero.button"
+                v-model="formDataTest.data.hero.button"
                 type="text"
-                :placeholder="formData.data.hero.button"
+                :placeholder="formDataTest.data.hero.button"
               />
             </div>
 
@@ -47,9 +59,9 @@
               <label class="text-lg font-medium" for>Hero button alt</label>
               <input
                 class="border border-blue-50 rounded px-3 py-2"
-                v-model="formData.data.hero.button_alt"
+                v-model="formDataTest.data.hero.button_alt"
                 type="text"
-                :placeholder="formData.data.hero.button_alt"
+                :placeholder="formDataTest.data.hero.button_alt"
               />
             </div>
           </div>
@@ -75,9 +87,12 @@ definePageMeta({
   middleware: 'auth'
 });
 
-const formData = reactive({
+const file = ref(null)
+
+const formDataTest = reactive({
   slug: page.value.slug,
   title: page.value.title,
+  image: "",
   data: {
     hero: {
       title: page.value.data.hero.title,
@@ -87,16 +102,27 @@ const formData = reactive({
   },
 })
 
-async function updatePage() {
-  const { refresh } = await useFetch(`http://localhost:3333/api/v1/pages/${route.params.slug}`, {
-    method: "PATCH",
-    body: formData
-  })
-
-  refresh()
+function handleFileUpload() {
+  console.log(file.value.files[0])
 }
 
-function assignData() {
-  formData.slug = page.value.slug
+async function updatePage() {
+  var formData = new FormData();
+
+  formData.append("file", file.value.files[0])
+  formData.append("slug", formDataTest.slug)
+  formData.append("title", formDataTest.title)
+  formData.append("data", JSON.stringify(formDataTest.data))
+
+  console.log(formData.get('file'));
+
+  const { data, refresh } = await useFetch(`http://localhost:3333/api/v1/pages/${route.params.slug}`, {
+    method: "PATCH",
+    body: formData,
+  })
+
+  console.log(data.value)
+
+  refresh()
 }
 </script>
