24d2ea8b23b5f562661546b645800106e2f18429
citingdude <glenn.reumers@hotmail.com>
2022-04-09 21:34:02 +0200

    Decrease font-size

--- components/cases/CasesBadge.vue
+++ components/cases/CasesBadge.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="badge">
+    <div
+        class="bg-dark-300 text-light-main max-w-max py-1 px-2 rounded-tl-lg rounded-br-lg font-body text-sm"
+    >
         <slot />
     </div>
 </template>
