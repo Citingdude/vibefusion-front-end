9a61f363a64060bb8b86bf69fafe41bc646aed28
citingdude <glenn.reumers@hotmail.com>
2022-04-10 11:34:49 +0200

    Create TipTapButton component

---
+++ components/TipTapButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="px-2 py-1 text-xs bg-accent-light rounded-md border-2  border-transparent hover:border-accent transition-colors" v-on:click="eventType">
+    <slot />
+  </button>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  clickEvent: {
+    type: String,
+    default: ""
+  },
+
+  editor: {
+    type: Object
+  }
+})
+
+const eventType = computed(() => {
+  switch (props.clickEvent) {
+    case 'bold':
+      return props.editor.chain().focus().toggleBold().run()
+
+    case 'h1':
+      return props.editor.chain().focus().setHeading({ level: 1 }).run()
+
+    case 'h2':
+      return props.editor.chain().focus().setHeading({ level: 2 }).run()
+  }
+})
+
+</script>
