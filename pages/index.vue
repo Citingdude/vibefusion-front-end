474833d6eaf2d009b280670238efdbac1f4c1582
citingdude <glenn.reumers@hotmail.com>
2022-04-10 11:01:41 +0200

    Fetch homepage data

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,12 @@
     <HomeHero />
     <HomeDiensten />
     <HomeCases />
-    <HomeCta />
+    <HomeCta :home="home" />
   </div>
 </template>
 
 <script setup>
+const { data:home } = await useAsyncData("home", () =>
+  $fetch("http://localhost:3333/api/v1/pages/home")
+);
 </script>
