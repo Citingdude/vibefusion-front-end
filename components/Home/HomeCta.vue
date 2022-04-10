07c8eecf5683a201c2e712bff6a72326519911b0
citingdude <glenn.reumers@hotmail.com>
2022-04-10 11:04:38 +0200

    Add api data for cta section

--- components/Home/HomeCta.vue
+++ components/Home/HomeCta.vue
@@ -1,20 +1,33 @@
 <template>
   <section class="flex section-padding-x bg-dark-300 justify-center">
     <div class="flex flex-col py-20 content-container">
+      <!-- Heading -->
       <div class="flex flex-col mb-4">
-        <h2
+        <div
+          v-html="home.content.cta_banner.title"
           class="text-3xl md:text-4xl lg:text-5xl font-bold font-display mb-3 text-light-main"
-        >Samenwerken voor jouw volgend project?</h2>
+        ></div>
       </div>
-      <p class="font-body text-light-alt mb-8 text-base md:text-lg lg:text-xl">
-        Heb je nood aan een nieuwe
-        <NuxtLink to="/webdesign" class="text-accent link-hover-border ease-in">website</NuxtLink>,
-        <NuxtLink to="/webshop" class="text-accent link-hover-border">webshop </NuxtLink>of
-        <NuxtLink to="/branding" class="text-accent link-hover-border">branding</NuxtLink>?
-        <br />We helpen je graag verder met jouw toekomstige projecten!
-      </p>
 
-      <AppButton color="transparent-alt" to="contact">Vraag een vrijblijvend gesprek aan</AppButton>
+      <!-- Body -->
+      <div
+        v-html="home.content.cta_banner.body"
+        class="font-body text-light-alt mb-8 text-base md:text-lg lg:text-xl max-w-xl"
+      ></div>
+
+      <!-- Button -->
+      <AppButton color="transparent-alt" to="contact">
+        {{ home.content.cta_banner.button }}
+      </AppButton>
     </div>
   </section>
 </template>
+
+<script setup>
+defineProps({
+  home: {
+    type: Object,
+    default: () => { }
+  }
+})
+</script>
