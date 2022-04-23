8b0504463480a936f516a0dbe554ad0accf56df9
citingdude <glenn.reumers@hotmail.com>
2022-04-23 10:19:35 +0200

    Enable diensten section

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <HomeHero :home="home.data.attributes" />
-    <!-- <HomeDiensten /> -->
+    <HomeDiensten :home=home.data.attributes />
     <!-- <HomeCases /> -->
     <!-- <HomeCta /> -->
   </div>
