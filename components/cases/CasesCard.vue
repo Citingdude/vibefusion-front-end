1a1ef3bad4bbcb0b429cbf343e2f16900841ec6c
citingdude <glenn.reumers@hotmail.com>
2022-04-28 14:39:07 +0200

    Fix render issue with v-html for summary

--- components/cases/CasesCard.vue
+++ components/cases/CasesCard.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center mb-12">
-    <img
-      class="w-full rounded-tl-3xl rounded-br-3xl mb-8"
-      :class="imageOrder"
-      src="~/assets/images/explauradise.jpg"
-      alt="Mockup van de Explauradise case"
-    />
+    <img class="w-full rounded-tl-3xl rounded-br-3xl mb-8" :class="imageOrder" src="~/assets/images/explauradise.jpg"
+      alt="Mockup van de Explauradise case" />
 
     <div class="flex flex-col px-8" :class="contentOrder">
       <CasesBadge v-if="badgeText">{{ badgeText }}</CasesBadge>
 
-      <h3 class="font-display font-bold text-2xl lg:text-3xl text-dark-400 my-6">{{ title }}</h3>
+      <h3 class="font-display font-bold text-2xl lg:text-3xl text-dark-400 my-6">
+        {{ title }}
+      </h3>
 
-      <div v-if="summary" class="mb-12 text-dark-300 font-body text-base md:text-lg lg:text-xl max-w-lg">
-        <p v-html="summary"></p>
+      <div v-if="summary" class="
+          mb-12
+          text-dark-300
+          font-body
+          text-base
+          md:text-lg
+          lg:text-xl
+          max-w-lg
+        ">
+        <div v-html="summary"></div>
       </div>
 
       <AppButton color="transparent" :to="link">Lees de volledige case</AppButton>
@@ -25,40 +31,39 @@
 defineProps({
   badgeText: {
     type: String,
-    default: 'badge'
+    default: "badge",
   },
 
   title: {
     type: String,
-    default: 'Title'
+    default: "Title",
   },
 
   summary: {
     type: String,
-    default: 'Summary'
+    default: "Summary",
   },
 
   image: {
     type: String,
-    default: 'Image'
+    default: "Image",
   },
 
   link: {
     type: String,
-    default: '/'
+    default: "/",
   },
 
   imageOrder: {
-    type: String,
-    default: 'order-1'
+    type: Object,
+    default: "{ order-1 }",
   },
 
   contentOrder: {
-    type: String,
-    default: 'order-2'
-  }
-})
-
+    type: Object,
+    default: "{ order-2 }",
+  },
+});
 </script>
 
 <script>
@@ -71,12 +76,12 @@ export default {
     buttonLink: Object,
     imageOrder: {
       type: String,
-      default: 'order-1',
+      default: "order-1",
     },
     contentOrder: {
       type: String,
-      default: 'order-2',
-    }
+      default: "order-2",
+    },
   },
-}
+};
 </script>
