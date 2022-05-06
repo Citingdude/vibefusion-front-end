8b70fe99482d4c19008a207161e5e56f27d36dc3
Glenn <glenn.reumers@hotmail.com>
2022-05-06 17:10:01 +0200

    Add hero section

--- pages/webdesign.vue
+++ pages/webdesign.vue
@@ -1,6 +1,35 @@
 <template>
+  <!-- Hero -->
+  <section
+    class="container mx-auto pt-16 lg:pt-32 pb-20 lg:pb-40 bg-left bg-no-repeat bg-cover section-padding-x grid lg:grid-cols-2 gap-16"
+  >
+    <!-- Content -->
   <div>
-    Webdesign
+      <h1 class="text-4xl lg:text-5xl font-bold font-display mb-4">
+        Scoor online met een conversiegerichte website
+      </h1>
+
+      <p class="mb-12">
+        Een professionele business website wordt een steeds belangrijker
+        digitaal kanaal dat voor klanten zorgt. Start vandaag nog met de
+        digitalisering van je bedrijf en zorg er voor dat jouw klanten je ook
+        online kunnen vinden.
+      </p>
+
+      <!-- Buttons -->
+      <div
+        class="flex flex-col xl:flex-row space-y-4 xl:space-y-0 xl:space-x-8"
+      >
+        <AppButton>Ontdek onze kwaliteiten</AppButton>
+        <AppButton color="transparent">Vraag een gesprek aan</AppButton>
+      </div>
+    </div>
+
+    <!-- Illustration -->
+    <div>
+      <img src="~/assets/images/webdesign-illustratie.svg" alt="" />
+    </div>
+  </section>
   </div>
 </template>
 
