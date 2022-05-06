8fbddba3fd8f80847dfc6b4b36bb53aeafc858a5
Glenn <glenn.reumers@hotmail.com>
2022-05-06 17:10:40 +0200

    Create features section

--- pages/webdesign.vue
+++ pages/webdesign.vue
@@ -4,7 +4,7 @@
     class="container mx-auto pt-16 lg:pt-32 pb-20 lg:pb-40 bg-left bg-no-repeat bg-cover section-padding-x grid lg:grid-cols-2 gap-16"
   >
     <!-- Content -->
-  <div>
+    <div>
       <h1 class="text-4xl lg:text-5xl font-bold font-display mb-4">
         Scoor online met een conversiegerichte website
       </h1>
@@ -30,8 +30,53 @@
       <img src="~/assets/images/webdesign-illustratie.svg" alt="" />
     </div>
   </section>
-  </div>
+
+  <!-- Features -->
+  <section class="pt-16 lg:pt-32 pb-20 lg:pb-40 section-padding-x bg-light-alt">
+    <div class="container mx-auto">
+      <!-- Heading -->
+      <div class="flex flex-col items-center w-full mb-8">
+        <h2 class="text-4xl lg:text-5xl font-bold font-display mb-4">
+          Waarom VibeFusion?
+        </h2>
+
+        <IconsLine />
+      </div>
+
+      <!-- Features grid -->
+      <div class="grid grid-cols-3">
+        <FeaturesCard
+          icon="rocket"
+          title="Topperformance"
+          body="Een snelle website wordt steeds belangrijker voor zoekmachines zoals Google. We optimaliseren jouw website grondig voor een optimale performance. Elke website wordt ook uitvoerig getest met de bekende Pagespeed test van Google."
+        />
+        <FeaturesCard
+          icon="target"
+          color="light"
+          title="Conversiegericht"
+          body="We starten al onze projecten met de vraag wat het doel van de website is. Zo kunnen we de website doelgericht opbouwen. We stellen ook de juiste Analytics in voor jouw website zodat je eenvoudig belangrijke data zoals conversies en bezoekers kan meten."
+        />
+        <FeaturesCard
+          icon="search"
+          title="SEO optimalisatie"
+          body="Naast de snelheid van een website zijn er nog een aantal technische aspecten die een impact hebben op de vindbaarheid van je website. We zorgen er voor dat deze tip top in orde zijn zodat Google jouw website niet afstraft."
+        />
+        <FeaturesCard
+          icon="brush"
+          color="light"
+          title="Custom design"
+          body="Elke website die wij bouwen start vanuit een wit blad. We zijn niet afhankelijk van beperkte templates of functionaliteiten en maken jouw website uniek en conversiegericht. Zo onderscheiden we jouw website van de concurrentie."
+        />
+        <FeaturesCard
+          icon="phone"
+          title="Mobielvriendelijk"
+          body="Een mobielvriendelijke website is tegenwoordig een must. Wanneer je website niet mobielvriendelijk is, zal Google je genadeloos straffen in de vindbaarheid van je website. Daarom bouwen we elke website op met de nieuwste responsieve technieken.
+"
+        />
+        <FeaturesCard type="cta" color="dark" title="Samenwerken?" />
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
