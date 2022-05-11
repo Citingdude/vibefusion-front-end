6b26729983d7bc15902c7f8fe5e9ae05fe426fb5
citingdude <glenn.reumers@hotmail.com>
2022-05-11 21:06:10 +0200

    Add roadmap section

--- pages/webdesign.vue
+++ pages/webdesign.vue
@@ -114,6 +114,20 @@
       </div>
     </div>
   </section>
+
+  <!-- Roadmap -->
+  <section class="pt-16 lg:pt-32 pb-20 lg:pb-40 section-padding-x bg-light-alt">
+    <div class="container mx-auto">
+      <!-- Heading -->
+      <div class="flex flex-col items-center w-full mb-16">
+        <h2 class="text-4xl lg:text-5xl font-bold font-display mb-4">
+          Hoe verloopt het proces?
+        </h2>
+
+        <IconsLine />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
