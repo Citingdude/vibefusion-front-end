f1aebf5942c8fe6a0bad24f6dd54c277cc7c9558
citingdude <glenn.reumers@hotmail.com>
2022-05-12 22:23:19 +0200

    Decomponize cta section

--- pages/index.vue
+++ pages/index.vue
@@ -98,7 +98,30 @@
         </div>
       </div>
     </section>
-    <HomeCta :home="home.data.attributes" />
+
+    <!-- Cta -->
+    <section class="flex section-padding-x bg-dark-300 justify-center">
+      <div class="flex flex-col py-20 content-container">
+        <!-- Heading -->
+        <div class="flex flex-col mb-4">
+          <div
+            v-html="home.data.attributes.cta.title"
+            class="text-3xl md:text-4xl lg:text-5xl font-bold font-display mb-3 text-light-main"
+          ></div>
+        </div>
+
+        <!-- Body -->
+        <div
+          v-html="home.data.attributes.cta.body"
+          class="font-body text-light-alt mb-8 text-base md:text-lg lg:text-xl max-w-xl"
+        ></div>
+
+        <!-- Button -->
+        <AppButton color="transparent-alt" :to="home.data.attributes.cta.button.url">
+          {{ home.data.attributes.cta.button.title }}
+        </AppButton>
+      </div>
+    </section>
   </div>
 </template>
 
