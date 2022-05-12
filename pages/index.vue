ab3dfcb6e3e1147fa54b2365d39794a1fbc72f79
citingdude <glenn.reumers@hotmail.com>
2022-05-12 22:19:19 +0200

    Decomponize hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,42 @@
 <template>
   <div>
-    <HomeHero :home="home.data.attributes" />
+    <!-- Hero -->
+    <section
+      class="container mx-auto pt-20 lg:pt-32 lg:pb-40 bg-left bg-no-repeat bg-cover section-padding-x"
+    >
+      <div class="content-container items-center">
+        <div class="grid grid-cols-2 lg:grid-cols-12 gap-12">
+          <!-- Content -->
+          <div
+            class="col-span-2 lg:col-span-6 max-w-full items-center anim-fade-in-left"
+          >
+            <!-- Heading -->
+            <div
+              v-html="home?.data.attributes.Hero?.Title"
+              class="text-6xl lg:text-7xl font-display mb-16 lg:mb-24"
+            ></div>
+
+            <!-- Buttons -->
+            <div
+              class="flex flex-col md:flex-row items-center md:items-start gap-8"
+            >
+              <AppButton hashlink="onze-diensten" size="large">
+                {{ home?.data.attributes.Hero?.button?.title }}
+              </AppButton>
+
+              <AppButton hashlink="cases" size="large" color="transparent">
+                {{ home?.data.attributes.Hero?.button_alt?.title }}
+              </AppButton>
+            </div>
+          </div>
+
+          <!-- Illustration -->
+          <div class="col-span-2 lg:col-span-6 anim-fade-in-right">
+            <IllustrationsHomeHero />
+          </div>
+        </div>
+      </div>
+    </section>
     <HomeDiensten :home="home.data.attributes" />
     <HomeCases :cases="cases" />
     <HomeCta :home="home.data.attributes" />
@@ -36,15 +72,18 @@ const { data: home } = await useAsyncData("home", () =>
   $fetch(`${apiBase}/homepage?${query}`)
 );
 
-const caseQuery = qs.stringify({
-  populate: '*'
-}, {
-  encodeValuesOnly: true,
-})
+const caseQuery = qs.stringify(
+  {
+    populate: "*",
+  },
+  {
+    encodeValuesOnly: true,
+  }
+);
 
 const { data } = await useAsyncData("cases", () =>
   $fetch(`${apiBase}/cases?${caseQuery}`)
 );
 
-const cases = data.value.data.slice(0, 2)
+const cases = data.value.data.slice(0, 2);
 </script>
