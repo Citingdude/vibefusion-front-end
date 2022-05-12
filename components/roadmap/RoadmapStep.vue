cbfe80ce3cb5f2f7b93139029030a3f442496a96
citingdude <glenn.reumers@hotmail.com>
2022-05-12 23:21:39 +0200

    Create RoadmapStep

---
+++ components/roadmap/RoadmapStep.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class="flex flex-row items-center space-x-16"
+    :class="{
+      'flex-row-reverse space-x-reverse': props.direction === 'reverse',
+    }"
+  >
+    <!-- Content -->
+    <div class="max-w-lg">
+      <h3 class="text-3xl font-display font-bold mb-4">
+        {{ props.title }}
+      </h3>
+
+      <p class="text-lg">
+        {{ props.content }}
+      </p>
+    </div>
+
+    <!-- Number -->
+    <div
+      class="flex justify-center items-center p-4 bg-light-main rounded-full w-16 h-16"
+    >
+      <p class="text-4xl font-bold font-display text-accent">
+        {{ props.number }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  direction: {
+    type: String,
+    default: "normal",
+  },
+
+  title: {
+    type: String,
+    default: "Step title",
+  },
+
+  content: {
+    type: String,
+    default: "Step content",
+  },
+
+  number: {
+    type: String,
+    default: "1",
+  },
+});
+</script>
