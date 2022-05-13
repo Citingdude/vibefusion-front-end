2cb9f9648c1b312986e3c5183c1e3575d4ad31a2
citingdude <glenn.reumers@hotmail.com>
2022-05-13 21:16:17 +0200

    Create FaqAccordion component

---
+++ components/faq/FaqAccordion.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="bg-dark-300 p-6 space-y-2 rounded-tr-3xl rounded-bl-3xl sm:p-16 sm:rounded-tr-8xl sm:space-y-4 sm:rounded-bl-8xl"
+  >
+    <FaqAccordionQuestion
+      :open="true"
+      question="Hoeveel kost een website?"
+      answer="De prijs van een WordPress website is sterk afhankelijk van de gewenste functionaliteiten en het aantal pagina’s. Bij VibeFusion start de prijs van een website vanaf 2.000 euro."
+    />
+    <FaqAccordionQuestion
+      question="Hoelang duurt het bouwen van een website?"
+      answer="Afhankelijk van de grootte en functionaliteiten van de website mikken we voor de gemiddelde WordPress website op 4 weken. De snelheid van het aanleveren van de content heeft hier ook een grote invloed op. Hoe sneller we de content aangeleverd krijgen hoe sneller we kunnen beginnen."
+    />
+    <FaqAccordionQuestion
+      question="Wat is een CMS-systeem?"
+      answer="Ja, het is perfect mogelijk om je eigen website volledig zelf te beheren. Wij bieden een aantal mogelijkheden aan in de mate van het beheer omdat niet elk bedrijf evenveel in eigen  beheer wilt hebben. We raden het wel af om zelf structurele aanpassingen te doen aan je website zonder kennis van html, css en javascript."
+    />
+    <FaqAccordionQuestion
+      question="Kan ik zelf mijn website beheren?"
+      answer="Ja, het is perfect mogelijk om je eigen website volledig zelf te beheren. Wij bieden een aantal mogelijkheden aan in de mate van het beheer omdat niet elk bedrijf evenveel in eigen  beheer wilt hebben. We raden het wel af om zelf structurele aanpassingen te doen aan je website zonder kennis van html, css en javascript."
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/faq/FaqAccordionQuestion.vue
@@ -0,0 +1,45 @@
+<template>
+  <div @click="open = !open" class="cursor-pointer">
+    <!-- Question -->
+    <div
+      class="flex space-x-4 items-center hover:bg-dark-200 transition rounded-full p-4"
+    >
+      <IconsArrow
+        class="transition min-w-max overflow-visible"
+        :class="{ 'rotate-90': open }"
+      />
+
+      <h3
+        class="text-light-main font-display font-semibold text-lg sm:text-2xl lg:text-3xl"
+      >
+        {{ props.question }}
+      </h3>
+    </div>
+
+    <!-- Answer -->
+    <div v-if="open">
+      <p class="text-light-alt max-w-3xl p-4 sm:text-lg">
+        {{ props.answer }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  question: {
+    type: String,
+    default: "Question",
+  },
+
+  answer: {
+    type: String,
+    default: "Answer",
+  },
+
+  open: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
