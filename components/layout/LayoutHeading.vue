1250219949c65677713d7edcc6415f22f3766810
Glenn <glenn.reumers@hotmail.com>
2022-05-15 14:41:18 +0200

    Add size variant

--- components/layout/LayoutHeading.vue
+++ components/layout/LayoutHeading.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="flex flex-col w-full">
-    <h2 class="text-4xl lg:text-5xl font-bold font-display mb-4">
+    <h2 v-if="props.size === 'h2'" class="text-4xl lg:text-5xl font-bold font-display mb-4">
       {{ props.title }}
     </h2>
+
+    <h3 v-if="props.size === 'h3'" class="text-3xl lg:text-4xl font-bold font-display mb-3">
+      {{ props.title }}
+    </h3>
 
     <IconsLine />
   </div>
@@ -13,6 +17,11 @@ const props = defineProps({
   title: {
     type: String,
     default: "Section title"
+  },
+
+  size: {
+    type: String,
+    default: "h2"
   }
 })
 
