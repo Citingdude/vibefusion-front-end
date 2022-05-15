fe53dd766a23746e7ab7316c07017ec867176001
Glenn <glenn.reumers@hotmail.com>
2022-05-15 14:20:18 +0200

    Add hero section

--- pages/branding.vue
+++ pages/branding.vue
@@ -1,8 +1,37 @@
 <template>
   <div>
-    Branding
+    <!-- Hero -->
+    <section class="pt-16 lg:pt-32 pb-20 lg:pb-40 section-padding-x">
+      <div class="container mx-auto grid lg:grid-cols-2 gap-16">
+        <!-- Content -->
+        <div>
+          <h1 class="text-4xl lg:text-5xl font-bold font-display mb-4">
+            Overtuig je klanten met een sterke branding
+          </h1>
+
+          <p class="mb-12 text-lg">
+            Een goede branding is meer dan alleen een logo. Het bestaat uit
+            verschillende elementen die elkaar versterken. Samen hebben ze het
+            doel om jouw merk te onderscheiden van de concurrentie en je bedrijf
+            te positioneren voor de juiste doelgroep.
+            <br /><br />
+            Wij helpen je verder met het ontwerp van een krachtige branding die
+            jouw bedrijf naar een hoger niveau tilt.
+          </p>
+
+          <!-- Button -->
+          <div class="flex">
+            <AppButton hashlink="branding-package">Ontdek het branding pakket</AppButton>
+          </div>
+        </div>
+
+        <!-- Illustration -->
+        <div>
+          <img src="~/assets/images/branding-illustration.svg" alt="" />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
