7383851f14d1f380a251c7499fcc557009943edc
Glenn <glenn.reumers@hotmail.com>
2022-05-15 14:58:54 +0200

    Create BenefitsItem component

---
+++ components/benefits/BenefitsItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex items-center space-x-8">
+    <!-- Icon -->
+    <IconsArrow />
+
+    <h3 v-if="props.type === 'normal'" class="text-light-alt text-2xl">
+      {{ props.title }}
+    </h3>
+
+    <NuxtLink
+      :to="to"
+      v-if="props.type === 'cta'"
+      class="text-light-main text-2xl hover:text-accent transition border-b border-accent"
+    >
+      {{ props.title }}
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: "normal",
+  },
+
+  to: {
+    type: String,
+  },
+
+  title: {
+    type: String,
+    default: "Benefit",
+  },
+});
+</script>
