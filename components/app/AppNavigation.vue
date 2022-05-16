adefa1d392d47e37d61b33a7fea0c3a1c6be2f7d
Glenn <glenn.reumers@hotmail.com>
2022-05-16 21:10:06 +0200

    Fix navigation

---
+++ components/app/AppNavigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav
+    class="hidden lg:flex justify-center items-center space-x-10 col-span-6 font-display font-medium text-xl"
+  >
+    <NuxtLink to="/">Home</NuxtLink>
+
+    <div @click="toggleDropdown()" class="relative cursor-pointer">
+      <p>Diensten</p>
+      <div
+        v-if="dienstenDropdown"
+        class="absolute flex flex-col top-full p-3 bg-light-alt rounded-xl space-y-3 w-max"
+      >
+        <AppNavigationLink
+          title="Website laten maken"
+          icon="webdesign"
+          to="/webdesign"
+        />
+        <AppNavigationLink
+          title="Webshop laten maken"
+          icon="webshop"
+          to="webshop"
+        />
+        <AppNavigationLink title="Branding" icon="branding" to="branding" />
+      </div>
+    </div>
+
+    <NuxtLink to="/cases">Cases</NuxtLink>
+
+    <NuxtLink to="/blog">Blog</NuxtLink>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let dienstenDropdown = ref(false);
+
+function toggleDropdown() {
+  document.removeEventListener("click", closeDropdown);
+  dienstenDropdown.value = !dienstenDropdown.value;
+  setTimeout(() => setCloseEvent(), 100);
+}
+
+function closeDropdown() {
+  dienstenDropdown.value = false;
+  document.removeEventListener("click", closeDropdown);
+}
+
+function setCloseEvent() {
+  document.addEventListener("click", closeDropdown);
+}
+</script>
---
+++ components/app/AppNavigationLink.vue
@@ -0,0 +1,68 @@
+<template>
+  <NuxtLink
+    :to="props.to"
+    class="flex space-x-4 items-center hover:bg-light-main p-4 rounded-xl"
+  >
+    <component :is="getIcon" class="w-12 h-12" />
+    <p :to="props.to">
+      {{ props.title }}
+    </p>
+  </NuxtLink>
+</template>
+
+<script setup>
+import { defineAsyncComponent, computed } from "vue";
+
+const WebdesignIcon = defineAsyncComponent(() => {
+  return new Promise((resolve) => {
+    resolve(import("~/components/icons/IconsWebdesign.vue"));
+  });
+});
+
+const WebshopIcon = defineAsyncComponent(() => {
+  return new Promise((resolve) => {
+    resolve(import("~/components/icons/IconsWebshop.vue"));
+  });
+});
+
+const BrandingIcon = defineAsyncComponent(() => {
+  return new Promise((resolve) => {
+    resolve(import("~/components/icons/IconsBranding.vue"));
+  });
+});
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: "Link",
+  },
+
+  icon: {
+    type: String,
+    default: "webdesign",
+  },
+
+  to: {
+    type: String,
+    default: "/",
+  },
+});
+
+const getIcon = computed(() => {
+  switch (props.icon) {
+    case "webdesign":
+      return WebdesignIcon;
+
+    case "webshop":
+      return WebshopIcon;
+
+    case "branding":
+      return BrandingIcon;
+
+    default:
+      return WebdesignIcon;
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
