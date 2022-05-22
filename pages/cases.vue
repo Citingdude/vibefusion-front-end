b8fcdcc8a57cb8579108f17d3716e0b2d0c87ea5
Glenn <glenn.reumers@hotmail.com>
2022-05-22 11:30:41 +0200

    Add cases api

--- pages/cases.vue
+++ pages/cases.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    Cases
+    <div>Cases</div>
+
+    <div>
+      {{ cases }}
+    </div>
   </div>
 </template>
 
 <script setup>
+import caseQuery from "~/queries/cases";
+
+const apiBase = useApiBase();
+
+const { data: cases } = await useAsyncData("cases", () =>
+  $fetch(`${apiBase}/cases?${caseQuery}`)
+);
 </script>
