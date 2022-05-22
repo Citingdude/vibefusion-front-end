742dd2b8f17ecd07c20cef387a3f8f2f5e0ea40c
Glenn <glenn.reumers@hotmail.com>
2022-05-22 12:01:15 +0200

    Create CasesCardCompact component

---
+++ components/cases/CasesCardCompact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- Image -->
+    <div>
+      <img class="rounded-tr-4xl" :src="imageUrl" alt="" />
+    </div>
+
+    <!-- Content -->
+    <div class="bg-dark-300 p-16 rounded-bl-4xl">
+      <h3 class="text-light-main text-4xl font-display mb-8 font-semibold">
+        {{ caseTitle }}
+      </h3>
+
+      <div class="flex">
+        <AppButton color="transparent-alt">Lees de case</AppButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  imageUrl: {
+    type: String,
+    default: "",
+  },
+
+  caseTitle: {
+    type: String,
+    default: "Case Title",
+  },
+});
+</script>
