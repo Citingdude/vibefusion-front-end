6fe0d00a725e481ffba4327a6875bc4913590c3d
Glenn <glenn.reumers@hotmail.com>
2022-06-24 10:56:48 +0200

    Create contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,49 @@
 <template>
   <div>
-    Contact
+    <section class="py-20 lg:py-40">
+      <div class="container">
+        <h1 class="mb-16 text-5xl font-bold md:text-6xl lg:text-7xl font-display">
+          Contacteer ons
+        </h1>
+
+        <!-- Content -->
+        <div class="grid lg:grid-cols-2">
+          <div class="p-12 sm:p-16 bg-dark-200 rounded-tl-4xl rounded-br-4xl w-max">
+            <h2
+              class="mb-8 text-3xl font-normal lg:text-4xl text-light-main font-display"
+            >
+              Contactgegevens
+            </h2>
+
+            <div class="space-y-8 text-light-alt">
+              <div class="flex items-center space-x-4">
+                <IconsArrow />
+                
+                <p>
+                  Meesstraat 7, <br />
+                  3950 Kaulille
+                </p>
+              </div>
+              <div class="flex items-center space-x-4">
+                <IconsArrow />
+                
+                <p>
+                  info@vibefusion.be
+                </p>
+              </div>
+              <div class="flex items-center space-x-4">
+                <IconsArrow />
+                
+                <p>
+                  0470 59 88 37
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
