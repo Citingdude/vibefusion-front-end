52280d3c7a89ab1ffb0b234b2d0fd1c86ad549c5
Glenn <glenn.reumers@hotmail.com>
2022-06-24 10:31:28 +0200

    Fix section padding and container

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-    <section
-      class="pt-20 lg:pt-32 lg:pb-40 bg-left bg-no-repeat bg-cover section-padding-x bg-light-alt"
-    >
-      <div class="container mx-auto">
-        <h1 class="font-display text-6xl font-bold mb-16">Onze blogs</h1>
+    <section class="py-20 md:py-40 bg-light-alt">
+      <div class="container">
+        <h1 class="mb-16 text-6xl font-bold font-display">Onze blogs</h1>
 
         <div class="grid grid-cols-3 gap-16">
           <BlogCard />
